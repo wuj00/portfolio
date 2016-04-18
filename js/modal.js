5157ca3ac18d7f5dc52728c8a6bd5e2ce4eb9283
Jennifer W <w.jennifer123@gmail.com>
2016-04-18 16:57:16 -0700

    added slides.js--made appropriate working changes

---
+++ js/modal.js
@@ -0,0 +1,22 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("modalImg");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    console.log(this.src);
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
